1d1faf65b731728af220dc086aa6270c9cff5909
claygilk <46429931+claygilk@users.noreply.github.com>
2024-03-08 11:13:16 -0500

    Update prompt.sh

--- prompt.sh
+++ prompt.sh
@@ -1,1 +1,14 @@
-prompt
+#
+# This function prints a prompt to the user in a pink color, then waits for their response.
+# The user input is then echoed so that it can be captured using command substitution.
+# Example: username=$(prompt "Enter your username: ")
+#
+# Usage: prompt [message]
+#
+
+function prompt(){
+  msg=$1
+  echo "\033[35m${msg}\033[0m\c" >&2
+  read response
+  echo $response
+}
