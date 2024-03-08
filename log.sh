532eb3ceb482a1e66e98d54adb8941238fb92d9d
claygilk <46429931+claygilk@users.noreply.github.com>
2024-03-08 10:43:00 -0500

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,1 +1,12 @@
-test
+#
+# This very simple logging function prints out a timestamp and a message.
+# The timestamp is colored purple.
+#
+# Usage: log [message]
+#
+
+function log(){
+  msg=$1
+  date=$(date "+%Y-%m-%dT%H:%M:%S")
+  echo "\033[34m${date}\033[0m ${msg} "
+}
