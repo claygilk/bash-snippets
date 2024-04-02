2b8f0b6591d50b8a50d0ef498c27c77cd2713ef4
claygilk <46429931+claygilk@users.noreply.github.com>
2024-04-02 13:27:47 -0400

    Update banner.sh

--- banner.sh
+++ banner.sh
@@ -1,1 +1,20 @@
-test
+#!/bin/sh
+
+#
+# This function prints a border around what ever string you pass to it.
+# The border is made of pound signs (#).
+#
+banner(){
+    msg=$1
+
+    char_count=$(printf "${msg}" | wc -c)
+    banner_length=$(( char_count + 12 ))
+
+    printf '#%.0s' $(seq $banner_length)
+    printf '\n'
+
+    printf "###   ${msg}   ###\n"
+
+    printf '#%.0s' $(seq $banner_length)
+    printf '\n'
+}
